81824404c7edc69be9c2dabdf2559eb4807fd148
rjw1428 <rjw1428@gmail.com>
2019-05-03 07:06:30 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ItemsService } from './shared/items.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  timeDelay = 10; //IN MINUTES
+  backgroundColor = 'black'
+  constructor(public service: ItemsService) {
+
+  }
+  ngOnInit() {
+    var time = Observable.interval(60000 * this.timeDelay)
+      .subscribe(x => {
+        var date = new Date()
+        if (date.getHours() == 4 && date.getMinutes() >= 0) {
+          console.log("COUNTDOWN TO REFRESH STARTED")
+          setTimeout(() => this.refresh(), 60000 * 100);
+        } else console.log(date);
+      })
+
+    this.service.getProperties().snapshotChanges().subscribe(vals => {
+      // vals.forEach(val => {
+      //   if (val.key == 'backgroundColor')
+      //     this.backgroundColor = val.payload.val()
+      // })
+    })
+  }
+
+  refresh() {
+    window.location.reload(true)
+  }
+
+  setBackgroundColor() {
+    let style = {
+      'background-color': this.backgroundColor
+    }
+    return style
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,114 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from './../environments/environment';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
 
 import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { ItemsService } from './shared/items.service';
+import { AbvPipe } from './shared/abv.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DisplayComponent } from './display/display.component';
+import { ItemEditComponent } from './item-edit/item-edit.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TitlecasePipe } from './shared/titlecase.pipe';
+import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { APP_BASE_HREF } from '@angular/common';
+import { DropdownDirective } from './shared/dropdown.directive';
+import { IconNamePipe } from './shared/icon-name.pipe';
+import { MenuComponent } from './menu/menu.component';
+import { FacebookModule } from 'ngx-facebook';
+import { PopupComponent } from './popup/popup.component';
+import { TextComponent } from './text/text.component';
+import { ContentManagerComponent } from './content-manager/content-manager.component';
+import { FoodEditComponent } from './food/food-edit/food-edit.component';
+import { UploadComponent } from './content-manager/upload/upload.component';
+import { UploadFormComponent } from './content-manager/upload-form/upload-form.component';
+import { MatInputModule, MatDialogModule, MatButtonModule, MatSelectModule, MatFormFieldModule, MatSnackBarModule, MatCheckboxModule, MatIconModule, MatAutocompleteModule } from '@angular/material';
+import { ContentService } from './shared/content.service';
+import { ScrollDispatchModule } from '@angular/cdk/scrolling';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FoodFormComponent } from './food/food-form/food-form.component';
+import { FoodItemComponent } from './food/food-item/food-item.component';
+import { CatagoryFormComponent } from './food/catagory-form/catagory-form.component';
+import { MenuService } from './shared/menu.service';
+import { CategoryComponent } from './food/category/category.component';
+import { CategoryEditComponent } from './food/category-edit/category-edit.component';
+import { ImagesComponent } from './images/images.component';
 
+const appRoutes: Routes = [
+  {
+    path: 'edit', component: MenuComponent, children: [
+      { path: 'drafts', component: MainComponent },
+      { path: 'menu', component: FoodEditComponent },
+      { path: 'content', component: ContentManagerComponent },
+      { path: 'output', component: TextComponent }
+    ]
+  },
+  { path: 'display/:screen', component: DisplayComponent },
+  { path: '**', redirectTo: 'edit/drafts', pathMatch: 'full' },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    AbvPipe,
+    DisplayComponent,
+    ItemEditComponent,
+    TitlecasePipe,
+    IconNamePipe,
+    MainComponent,
+    DropdownDirective,
+    MenuComponent,
+    PopupComponent,
+    TextComponent,
+    ContentManagerComponent,
+    FoodEditComponent,
+    UploadComponent,
+    UploadFormComponent,
+    FoodFormComponent,
+    FoodItemComponent,
+    CatagoryFormComponent,
+    CategoryComponent,
+    CategoryEditComponent,
+    ImagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule,
+    AngularFireDatabaseModule,
+    AngularFireStorageModule,
+    AngularFirestoreModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    FacebookModule.forRoot(),
+    MatInputModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatSnackBarModule,
+    MatIconModule,
+    MatCheckboxModule,
+    ScrollDispatchModule,
   ],
-  providers: [],
+  entryComponents: [
+    UploadFormComponent,
+    FoodFormComponent,
+    CatagoryFormComponent,
+    CategoryEditComponent
+  ],
+  providers: [ItemsService, ContentService, MenuService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
